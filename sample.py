b8c8a6125a993aeed87a2b0ed5e9ec9f41e7374b
jacks1919 <46815705+jacks1919@users.noreply.github.com>
2019-03-29 12:40:23 +0000

    Add files via upload
    Added Facial recognition where sample.py will run as a daemon and take a picture if a face is detected.

--- sample.py
+++ sample.py
@@ -26,11 +26,12 @@ import time
 import cv2
 import os
 import pygame
+import threading
 
 
 ### Setup #####################################################################
 
-os.putenv('SDL_FBDEV', '/dev/fb1')
+#os.putenv('SDL_FBDEV', '/dev/fb1')
 
 # Setup the camera
 camera = PiCamera()
@@ -47,47 +48,57 @@ t_start = time.time()
 
 
 ### Main ######################################################################
-#class FacialRec():
+class FacialRec():
+    def __init__(self, interval=1):
+        
+        self.interval = interval
+        
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
     
-    #def get_FacialRecognition(self):
-        # Capture frames from the camera
-for frame in camera.capture_continuous( rawCapture, format="bgr", use_video_port=True ):
+    def run (self):
+    # Capture frames from the camera
+        for frame in camera.capture_continuous( rawCapture, format="bgr", use_video_port=True ):
 
-    image = frame.array
+            image = frame.array
 
-    # Look for faces and phones in the image using the loaded cascade file
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    faces  = face_cascade.detectMultiScale(gray)
-    phones = phone_cascade.detectMultiScale(gray)
+            # Look for faces and phones in the image using the loaded cascade file
+            gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+            faces  = face_cascade.detectMultiScale(gray)
+            phones = phone_cascade.detectMultiScale(gray)
 
-    # Draw a rectangle around every face
-    
-    
-    for (x,y,w,h) in faces:
-        cv2.rectangle( image, ( x, y ), ( x + w, y + h ), ( 255, 255, 0 ), 2 )
-        cv2.putText( image, "face detected" + str( len( faces ) ), ( x, y ), cv2.FONT_HERSHEY_SIMPLEX, 0.5, ( 0, 0, 255 ), 2 )
-        print('Face Detected')
-        #camera.resolution = ( 320, 240 )
-        #rawCapture = PiRGBArray( camera, size=( 320, 240 ) )
-        #camera.start_preview()
-        sleep(5)
-        camera.capture('/tmp/cameraTest.jpg')
-        #camera.stop_preview()
-        sleep(15)
+            # Draw a rectangle around every face
+            
+            
+            for (x,y,w,h) in faces:
+                cv2.rectangle( image, ( x, y ), ( x + w, y + h ), ( 255, 255, 0 ), 2 )
+                cv2.putText( image, "face detected" + str( len( faces ) ), ( x, y ), cv2.FONT_HERSHEY_SIMPLEX, 0.5, ( 0, 0, 255 ), 2 )
+                print('Face Detected')
+                #camera.resolution = ( 320, 240 )
+                #rawCapture = PiRGBArray( camera, size=( 320, 240 ) )
+                #camera.start_preview()
+                sleep(5)
+                camera.capture('/tmp/cameraTest.jpg')
+                #camera.stop_preview()
+                sleep(15)
+                
+                
 
-    '''# Draw a rectangle around every phone
-    for (x,y,w,h) in phones:
-        cv2.rectangle( image, ( x, y ), ( x + w, y + h ), ( 255, 0, 0 ), 2 )
-        cv2.putText( image, "iPhone", ( x, y ), cv2.FONT_HERSHEY_SIMPLEX, 0.5, ( 0, 255, 255 ), 2 )'''
+            '''# Draw a rectangle around every phone
+            for (x,y,w,h) in phones:
+                cv2.rectangle( image, ( x, y ), ( x + w, y + h ), ( 255, 0, 0 ), 2 )
+                cv2.putText( image, "iPhone", ( x, y ), cv2.FONT_HERSHEY_SIMPLEX, 0.5, ( 0, 255, 255 ), 2 )'''
 
-    # Calculate and show the FPS
-    '''fps = fps + 1
-    sfps = fps / ( time.time() - t_start )
-    cv2.putText( image, "FPS : " + str( int( sfps ) ), ( 10, 10 ), cv2.FONT_HERSHEY_SIMPLEX, 0.5, ( 0, 0, 255 ), 2 )'''
+            # Calculate and show the FPS
+            '''fps = fps + 1
+            sfps = fps / ( time.time() - t_start )
+            cv2.putText( image, "FPS : " + str( int( sfps ) ), ( 10, 10 ), cv2.FONT_HERSHEY_SIMPLEX, 0.5, ( 0, 0, 255 ), 2 )'''
 
-    cv2.imshow( "Frame", image )
-    cv2.waitKey( 1 )
+            cv2.imshow( "Frame", image )
+            cv2.waitKey( 1 )
 
-    # Clear the stream in preparation for the next frame
-    rawCapture.truncate( 0 )
+            # Clear the stream in preparation for the next frame
+            rawCapture.truncate( 0 )
+            #break
             
